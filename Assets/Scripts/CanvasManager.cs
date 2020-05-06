d49f95672cb0beaaf3c46299bbd0f55a78f9d0da
Sweets888 <michaelsweetman888@gmail.com>
2020-05-06 13:23:50 +1000

    Set up Instruction canvas access from title screen and vice versa

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject gameCamera;
+    public GameObject titleCamera;
+    public GameObject gameCanvas;
+    public GameObject titleCanvas;
+    public GameObject instructionCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+        gameCamera.SetActive(false);
+        titleCamera.SetActive(true);
+        gameCanvas.SetActive(false);
+        titleCanvas.SetActive(true);
+        instructionCanvas.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        gameCamera.SetActive(true);
+        titleCamera.SetActive(false);
+        gameCanvas.SetActive(true);
+        titleCanvas.SetActive(false);
+        instructionCanvas.SetActive(false);
+    }
+
+    public void ViewInstructions()
+    {
+        titleCanvas.SetActive(false);
+        instructionCanvas.SetActive(true);
+    }
+
+    public void BackToTitle()
+    {
+        titleCanvas.SetActive(true);
+        instructionCanvas.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
